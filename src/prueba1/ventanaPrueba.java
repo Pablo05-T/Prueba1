55aba3e7d436a5603f380ad3e69da4ab6c167669
Personal <Personal@DESKTOP-UV1IPK4>
2020-10-07 15:58:13 -0500

    1.1

---
+++ src/prueba1/ventanaPrueba.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba1;
+import javax.swing.*;
+/**
+ *
+ * @author Personal
+ */
+public class ventanaPrueba {
+    public static void main(String[] args){
+        JFrame f = new JFrame("Mi ventana");
+        f.setSize(400, 300);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+}
